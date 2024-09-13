7017f73743f48ce82bccafba46f8c67e37b4086a
danielgaim-visma <daniel.gaim@visma.com>
2024-09-13 22:36:07 +0200

    config.py

--- backend/config.py
+++ backend/config.py
@@ -12,11 +12,14 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 ALLOWED_EXTENSIONS = {'wav', 'mp3', 'flac'}
 
 # Google Cloud Storage bucket name
-BUCKET_NAME = 'lydtest75'
+BUCKET_NAME =
 
 # Other configuration settings...
 SECRET_KEY = 'your-secret-key-here'  # Replace with a real secret key
 DEBUG = True  # Set to False in production
 
 # Google Cloud credentials path
-GOOGLE_APPLICATION_CREDENTIALS = "/Users/daniel.gaimvisma.com/Downloads/test-flyta-cm-c0d5ab00c3ec.json"
+GOOGLE_APPLICATION_CREDENTIALS =
+
+# OpenAI API key
+OPENAI_API_KEY =
